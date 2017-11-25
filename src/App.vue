7d11dee14fdc072e0885d7c97238c71c0465a440
Mikhail Gorbachev <mgorbachev@factset.com>
2017-11-25 21:31:07 +0000

    Initial commit + progress bar

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app" class="bg-inverse text-white" style="min-height: 100%; height: 100%;">
+    <nav class="navbar navbar-fixed-top navbar-toggleable-md navbar-inverse bg-inverse">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="/">delay-sim</a>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="/about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'delay-sim'
+    }
+  },
+  components: {
+    // Components go here
+  }
+}
+</script>
+
+<style>
+  input:focus,
+  select:focus,
+  textarea:focus,
+  button:focus {
+    outline: none;
+  }
+
+  .navbar { margin-bottom: 50px; }
+
+  p {
+    margin: 0em;
+  }
+
+  p.lead {
+    font-weight: 500;
+    margin-bottom: 1em;
+    color: #888;
+  }
+
+  h1, h2 {
+    font-weight: 900;
+    margin-bottom: 1em;
+  }
+
+  h3, h4 {
+    margin-top: 1em;
+    font-weight: 700;
+    margin-bottom: 1em;
+  }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="bg-inverse text-white">
+    <div class="text-center">
+    </div>
+    <progress-bar time="5 min"></progress-bar>
+  </div>
+</template>
+
+<script>
+import ProgressBar from './ProgressBar';
+
+export default {
+  name: "home",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  components: {
+    'progress-bar': ProgressBar
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+#container {
+  max-width: 400px;
+  height: 400px;
+  margin: auto;
+}
+.header {
+  /* background: #292b2c; */
+}
+
+.oval1 {
+  background-image: linear-gradient(
+    -180deg,
+    #ffffff 0%,
+    #d1dbdc 49%,
+    #bbbbbb 100%
+  );
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.25), 0 1px 3px 0 rgba(0, 0, 0, 0.4),
+    inset 0 0 0 0 rgba(255, 255, 255, 0.16);
+}
+/* Oval 2: */
+.oval2 {
+  background-image: radial-gradient(
+    50% 97%,
+    #6bdb5b 23%,
+    #58dc39 30%,
+    #44bd3a 65%,
+    #309f24 98%
+  );
+}
+/* Rectangle-path: */
+.rectangle-path {
+  background-image: linear-gradient(-180deg, #f4faf8 33%, #d9d8d6 84%);
+  box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.36), 0 0 0 0 rgba(36, 36, 36, 0.32),
+    inset 0 0 1px 0 #ffffff;
+}
+</style>
